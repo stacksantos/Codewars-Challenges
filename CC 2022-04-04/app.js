6a5de69287d859e935d8fd68ae388f7a8b46bbf6
Leesvntos <christopherleesantos@gmail.com>
2022-04-02 17:09:47 -0500

    Add Codewars problem: Lost Without a Map

---
+++ CC 2022-04-04/app.js
@@ -0,0 +1,9 @@
+//Codewars problem: Lost Without a Map
+
+//Given an array of integers, return a new array with each value doubled.
+
+//For example:
+
+//[1, 2, 3] --> [2, 4, 6]
+
+//Q: Is it always going to be integers? are we going to be given any special characters? Are empty arrays ever given?
