227d9c873602c3e881a698c9c4509f314fa1ab90
Leesvntos <christopherleesantos@gmail.com>
2022-04-05 21:51:34 -0500

    Add Solution Workthrough

--- CC 2022-04-05/app.js
+++ CC 2022-04-05/app.js
@@ -7,3 +7,23 @@ a "Yes" string for true, or a "No" string for false.
 
 */
 
+//First create the function and insert the bool param because that's
+//what you'll be testing
+
+function boolToWord(bool) {
+    //create an "if/else" statement to check whether the bool is
+    //true or false so that you can assign the correct "word" to it
+    if (bool === true) {
+        //use the .toString method to convert the boolean to a string
+        //and return the value "Yes" for true
+        boolToWord.toString(true)
+        return "Yes"
+    } else {
+        //And with the boolean false return "No"
+        boolToWord.toString(false)
+        return "No"
+    }
+}
+
+//If you call boolToWord() without a boolean inserted it will
+//return false and "No" by default.
