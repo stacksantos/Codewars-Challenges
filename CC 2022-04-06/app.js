3331d1d20ba54f547281654244f182412d0d481f
Leesvntos <christopherleesantos@gmail.com>
2022-04-07 00:20:53 -0500

    Add Codewars kata

--- CC 2022-04-06/app.js
+++ CC 2022-04-06/app.js
@@ -28,9 +28,13 @@ const rps = (p1, p2) => {
 
 //Solution submitted to codewars
 
+/*
+
 const rps = (p1, p2) => {
     if (p1 === 'scissors' && p2 === 'paper' || p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock') {
         return 'Player 1 won!' } else if (p1 === p2) {
             return 'Draw!'} else {
                 return 'Player 2 won!'}
   };
+
+  */
