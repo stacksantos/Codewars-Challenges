1771c77edafd4c57e70024f8952d7d81d5da629a
Leesvntos <christopherleesantos@gmail.com>
2022-04-06 21:00:21 -0500

    Add Solution

--- CC 2022-04-06/app.js
+++ CC 2022-04-06/app.js
@@ -25,3 +25,12 @@ const rps = (p1, p2) => {
             //then all that's left is every other scenario where player 2 would have won.
              else {  return "Player 2 wins!"}
         };
+
+//Solution submitted to codewars
+
+const rps = (p1, p2) => {
+    if (p1 === 'scissors' && p2 === 'paper' || p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock') {
+        return 'Player 1 won!' } else if (p1 === p2) {
+            return 'Draw!'} else {
+                return 'Player 2 won!'}
+  };
