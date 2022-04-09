84c5a182f98d513daabb4daeb7b5f743577cb48f
Leesvntos <christopherleesantos@gmail.com>
2022-04-08 20:32:34 -0500

    add solution workthrough

--- CC 2022-04-08 Even or Odd/app.js
+++ CC 2022-04-08 Even or Odd/app.js
@@ -5,3 +5,16 @@
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 */
+
+//To solve this problem a simple if/else method can be used
+function even_or_odd(number) {
+  //first check to see if the number given is divisible by an even number like 2
+  // using the % character will give us the remainder of 2 numbers being divided
+  if (number % 2 === 0) {
+      //if it is divisible by 2 then it is even
+      return "Even"}
+      //otherwise it will return odd
+      else
+      return "Odd"
+  }
+  // we could use an else if method as well for all other edge cases but it's not necessary
