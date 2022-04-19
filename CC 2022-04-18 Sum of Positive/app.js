73cf50b5b943dd138ba1d9cfc61f65eaec820e18
Leesvntos <christopherleesantos@gmail.com>
2022-04-18 20:19:36 -0500

    add workthrough and solution

--- CC 2022-04-18 Sum of Positive/app.js
+++ CC 2022-04-18 Sum of Positive/app.js
@@ -9,3 +9,14 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
 
 */
+
+
+function positiveSum(arr) {
+    //first we create a variable to hold the sum of the positive integers
+    let sum = 0;
+    //then we create a forEach function to iterate through each number and check
+    //to see if it is positive (greater than 0).  && if it is add it to the sum.
+    arr.forEach(num => (num > 0) && (sum += num));
+    //lastly we return the sum.
+    return sum;
+  }
