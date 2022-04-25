15c589a9c35ebbf4b361496312653ecde2985f3b
Leesvntos <christopherleesantos@gmail.com>
2022-04-24 22:07:51 -0500

    add solution and walkthrough

--- CC 2022-04-24 Convert Number to Reversed Array of Digits/app.js
+++ CC 2022-04-24 Convert Number to Reversed Array of Digits/app.js
@@ -9,3 +9,21 @@ Example:
 0 => [0]
 
 */
+
+
+/*
+This was the original format for the question
+
+function digitize(n) {
+  //your code here
+}
+*/
+
+// I converted it to an arrow function to make it more streamline
+
+const digitize = n => n.toString(10).split("").map(Number).reverse();
+
+//first I used the toString method to convert the number into a string
+//then the split method separates them into individual strings
+//the map method will iterate for each item in the string and turn it back into a Number
+//and finally the whole thing is reversed
