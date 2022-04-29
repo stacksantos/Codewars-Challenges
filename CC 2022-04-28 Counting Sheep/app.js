5c1efa8293f5f1d71b9f120e169945efa5013a8d
stacksantos <stacksantos@proton.me>
2022-04-28 22:14:11 -0500

    add workthough

--- CC 2022-04-28 Counting Sheep/app.js
+++ CC 2022-04-28 Counting Sheep/app.js
@@ -21,12 +21,16 @@ Hint: Don't forget to check for bad values like null/undefined
 */
 
 function countSheeps(arrayOfSheep) {
+  //first I created a variable to hold the number of sheep that will later accumulate
   let numSheep = 0;
-  
+  //then I created a for loop to check every iteration of the array
   for (let i = 0; i <= arrayOfSheep.length; i++) {
+    //when checking every iteration, if that iteration was true then it would
     if (arrayOfSheep[i] === true) {
+      //add 1 to the sheep until the array was finished
       numSheep++
     }
   }
+  //always remember to return or you wont get a value back
   return numSheep
   }
