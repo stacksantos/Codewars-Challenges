fed2f427ca6c7797502a932b9c2bcdcd7a999631
stacksantos <stacksantos@proton.me>
2022-05-07 18:43:06 -0500

    add solution and workthrough

--- CC 2022-05-07 Sum without highest and lowest number/app.js
+++ CC 2022-05-07 Sum without highest and lowest number/app.js
@@ -20,5 +20,17 @@ If an empty value ( null, None, Nothing etc. ) is given instead of an array, or 
 
 
 function sumArray(array) {
-    
+    //First we have to check whether the array is valid or not.  So we'll check see 1) it is an array && 2) that array is more than 1 item.
+    if (array && array.length > 1) {
+        // then we have to sort the array and remove the first and last items.  Slice works best in this case because there are only 2 items to remove.
+        // do NOT forget to add the function within the sort or you'll get an error.
+        let sortedArr = array.sort((a, b) => a - b).slice(1, -1);
+        // then you have to ask for the array back with RETURN otherwise you'll get nothing back.
+        // now in order to add all of the array items together you'll use the reduce method.
+        // again DONT FORGET to use the function within reduce or you'll get an error.
+        return sortedArr.reduce((a, b) => a + b, 0);
+        // then everything ELSE we return 0;
+      } else
+        
+        return 0;
   };
