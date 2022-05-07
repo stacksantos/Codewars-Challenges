5d16b44a717ff44ea639a0c9eb17c937f423403a
stacksantos <stacksantos@proton.me>
2022-05-07 18:56:59 -0500

    add solution and workthrough

--- CC 2022-05-07 Grasshopper gradebook/app.js
+++ CC 2022-05-07 Grasshopper gradebook/app.js
@@ -13,5 +13,18 @@ Tested values are all between 0 and 100. Theres is no need to check for negative
 */
 
 function getGrade (s1, s2, s3) {
-    
+    //first we need to get the average of the 3 grades. Simple math here. Add them up then divide by however many there are.
+    let grade = (s1 + s2 + s3) / 3;
+    //then we need to add our if parameters for each grade letter
+    if (grade >= 90) {
+      return 'A'
+    } else if (grade >= 80 && grade < 90) {
+      return 'B'
+    } else if (grade >= 70 && grade < 80) {
+      return 'C'
+    } else if (grade >= 60 && grade < 70) {
+      return 'D'
+    } else if (grade >= 0 && grade < 60) {
+      return 'F'
+    }
   }
