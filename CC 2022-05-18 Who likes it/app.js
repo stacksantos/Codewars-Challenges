7b6aa231cd565e1177bbc43d32097447a9cb1797
stacksantos <stacksantos@proton.me>
2022-05-18 20:13:31 -0500

    add solution and workthrough

--- CC 2022-05-18 Who likes it/app.js
+++ CC 2022-05-18 Who likes it/app.js
@@ -17,7 +17,19 @@ Implement the function which takes an array containing the names of people that 
 
 
 
-
+// for this function we'll have to use if/else and concatenate strings
 function likes(names) {
-    // TODO
+    if (names.length === 0) {
+      return "no one likes this"
+    } else if (names.length === 1) {
+      return names[0] + " likes this"
+    } else if (names.length === 2) {
+        //don't forget to add the spaces so normal sentence structure is obtained
+      return names[0] + " and " + names[1] + " like this"
+    } else if (names.length === 3) {
+      return names[0] + ", " + names[1] + " and " + names[2] + " like this"
+    } else {
+        //for the last one to get the number you use names.length - 2 because we're including 2 names already so we need whatever is left after that
+      return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this"
+    }
   }
