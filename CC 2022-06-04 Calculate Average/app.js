8468208d27bf5589cdea6ffd345c76e6adfa0e5f
stacksantos <stacksantos@proton.me>
2022-06-04 10:55:30 -0500

    add solution and workthrough

--- CC 2022-06-04 Calculate Average/app.js
+++ CC 2022-06-04 Calculate Average/app.js
@@ -10,5 +10,14 @@ Note: Empty arrays should return 0.
 
 
 function find_average(array) {
-    //add code here
+    //first we need to reduce the array. Set the initial value to 0
+    let sum = 0;
+    let reduced = array.reduce((a,b) => a + b, sum);
+    //then we need to find the average, so we divide the sum by the number of items in the array
+    let result = (reduced / array.length);
+    //we need to make sure the array isn't empty
+    if (array !== "") {
+      return result
+      //otherwise we return 0
+    } else return 0;
   }
